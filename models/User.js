7dbc9d459c5891d8ff56afe9840972ea69180c16
Patrick <patrick.hurley@codeimmersives.com>
2020-06-16 09:27:53 -0400

    done

--- models/User.js
+++ models/User.js
@@ -1,18 +1,9 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// const UserSchema = new mongoose.Schema({
-  // name: {type:String, default:'', required:true},
-  // email:{type:String, unique:true, lowercase:true, default:'',required:true},
-  // password:{type:String,default:'',required:true},
-// })
+const UserSchema = new mongoose.Schema({
+  name: {type:String, default:'', required:true},
+  email:{type:String, unique:true, lowercase:true, default:'',required:true},
+  password:{type:String,default:'',required:true},
+})
 
-// var mongoose = require('mongoose');
-//   var Schema = mongoose.Schema;
-
-//   var UserSchema = new Schema({
-//     name: {type:String, default:'', required:true},
-//     email:{type:String, unique:true, lowercase:true, default:'',required:true},
-//     password:{type:String,default:'',required:true},
-//   });
-
-//   module.exports = mongoose.Schema('user', UserSchema)
+  module.exports = mongoose.model('user', UserSchema)
